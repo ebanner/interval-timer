a5df8cba89cb1d06ca227b49bd0df3f273c87cad
Edward Banner <edward.banner@gmail.com>
2023-03-31 15:20:41 -0400

    Initial Commit

---
+++ Watch Interval Timer Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Watch Interval Timer Watch App
+//
+//  Created by Edward Banner on 3/31/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Watch Interval Timer Watch App/Watch_Interval_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Watch_Interval_TimerApp.swift
+//  Watch Interval Timer Watch App
+//
+//  Created by Edward Banner on 3/31/23.
+//
+
+import SwiftUI
+
+@main
+struct Watch_Interval_Timer_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
